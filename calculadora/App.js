38beec1e4eccf9508ff76d3b4c44ab6c6bba7c92
luisgdev303 <luisg.dev.lu@gmail.com>
2026-02-27 11:39:00 -0300

    Luis G & Bressan Atualizado

--- calculadora/App.js
+++ calculadora/App.js
@@ -4,8 +4,15 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+
+      <View style={styles.displayContainer}>
+        <Text style={style.displayText} numberOfLines={1}>
+          67
+        </Text>
+      </View>
+
+
     </View>
   );
 }
@@ -13,8 +20,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#000000',
   },
+  displayContainer: {
+    flex: 2.5,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    paddingHorizontal: 20,
+    paddingBottom: 20,
+  },
+  displayText: {
+    color: '#ffffff',
+    fontSize: 72,
+    fontWeight: '300'
+  }
 });
